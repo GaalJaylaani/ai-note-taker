7eb1e66b7cc2743f13610216c2533eb0d3ad8850
gaaljaylaani <GAALJAYLAANI123@GMAIL.COM>
2025-11-04 17:27:43 -0800

    wrote auth.py

---
+++ app/api/v1/auth.py
@@ -0,0 +1,21 @@
+
+
+
+
+@router.post("/register", response_model=Token)
+def register(user: UserCreate, db: Session = Depends(get_db)):
+    exists = db.query(User).filter(User.email == user.email).first()
+    if exists:
+        raise HTTPException(status_code=400, detail = 'Email already registered')
+    u = User(email=user.email, password_hash=hash_password(user.password))
+    db.add(u)
+    db.commit()
+    db.refresh(u)
+    return {"access_token": create_access_token(str(u.id))}
+
+@router.post("/login", response_model=Token)
+def login(creds; UserCreate, db: Session = Depends(get_db)):
+    u = db.query(User).filter(User.email == creds.email).first()
+    if not u or not verify_password(creds.password, u.password_hash):
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid credentials")
+    return {"access_token": create_access_token(str(u.id))}
