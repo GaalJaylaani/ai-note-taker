2b5a12089799f7d72c1a94365667721d727ca6b1
gaaljaylaani <GAALJAYLAANI123@GMAIL.COM>
2025-11-07 10:32:47 -0800

    made settings insde config.py

---
+++ app/core.py/config.py
@@ -0,0 +1,11 @@
+import pydantic import BaseModel, Field
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+class Settings(BaseModel):
+    DATABASE_URL: str = Field(default=os.getenv("DATABASE_URL", "postgresql+psycopg://postgres:postgres@localhost:5432/ainotetaker"))
+    SECRET_KEY: str = Field(default=os.getenv("SECRET_KEY", "dev-secret"))
+    ACCESS_MIN: int = Field(default=int(os.getenv("ACCESS_MIN", "60")))
+
+settings = Settings()
