bd7b3f1df586c39d8cb6edfe396640fe24fc9fb3
gaaljaylaani <GAALJAYLAANI123@GMAIL.COM>
2025-11-08 12:37:23 -0800

    instead of hardcoded database url just used it from settings in config

--- app/core.py/database.py
+++ app/core.py/database.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm  import sessionmaker, declarative_base
+from app.core.config import settings
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./notes.db"
+SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
