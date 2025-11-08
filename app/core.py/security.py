b290b20cc298b996622345666638a4270e2093ba
gaaljaylaani <GAALJAYLAANI123@GMAIL.COM>
2025-11-08 12:37:55 -0800

    added get curr user id func

--- app/core.py/security.py
+++ app/core.py/security.py
@@ -5,6 +5,7 @@ from app.core.config import settings
 
 pwd_context = CryptContext(schemes=["pbkdf2_sha256"], deprecated="auto")
 ALGO = "HS256"
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
 
 def hash_password(password: str) -> str:
     return pwd_context.hash(password)
@@ -15,3 +16,12 @@ def verify_password(password: str, hashed: str) -> bool:
 def create_access_token(sub: str) -> str:
     expire = datetime.now(timezone.utc) + timedelta(minutes=settings.ACCESS_MIN)
     return jwt.encode({"sub": sub, "exp": expire}, settings.SECRET_KEY, algorithm=ALGO)
+
+def get_current_user_id(token: str = Depends(oauth2_scheme)) -> str:
+    try:
+        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[ALGO])
+        return payload.get("sub")
+    except jwt.JWTError:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token")
+
+        
