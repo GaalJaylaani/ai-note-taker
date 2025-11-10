eb5036785858e19e6a4461c971cdbba7fcad650e
gaaljaylaani <GAALJAYLAANI123@GMAIL.COM>
2025-11-10 14:15:07 -0800

    imported get_current_user instead of create_access_token

--- app/api/v1/auth.py
+++ app/api/v1/auth.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 from app.core.database import get_db
-from app.core.security import hash_password, verify_password, create_access_token
+from app.core.security import hash_password, verify_password, create_access_token, get_current_user
 from app.models.user import User
 from app.schemas.user import UserCreate, Token
 
