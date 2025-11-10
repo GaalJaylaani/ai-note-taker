38f56cb7ff1bb9c609ee0fba649819d2b34f1817
gaaljaylaani <GAALJAYLAANI123@GMAIL.COM>
2025-11-10 14:15:56 -0800

    made read notes validate user in database query instead of just id

--- app/api/v1/notes.py
+++ app/api/v1/notes.py
@@ -17,7 +17,7 @@ def create_note(payload: NoteCreate, db: Session = Depends(get_db)):
 
 @router.get("/", response_model=List[NoteRead])
 def read_notes(db: Session = Depends(get_db)):
-    notes = db.query(Note).filter(Note.owner == current_user.id).all()
+    notes = db.query(Note).filter(Note.owner == current_user).all()
     return notes
 
 @router.get("/{note_id}", response_model=NoteRead)
