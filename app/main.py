e9b2d827d3aadab3cdd7b202a85fa3875824a4da
gaaljaylaani <GAALJAYLAANI123@GMAIL.COM>
2025-11-10 14:14:08 -0800

    changed router to users instead of user

--- app/main.py
+++ app/main.py
@@ -1,13 +1,16 @@
 from fastapi import FastAPI
-from app.core.database import Base, engine
-from app.api.v1 import notes
+
+from .core.database import Base, engine
+import app.api.v1.auth as auth
+import app.api.v1.users as users
+import app.api.v1.notes as notes
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="AI Note Taker")
 
 app.include_router(notes.router)
-app.include_router(user.router)
+app.include_router(users.router)
 app.include_router(auth.router)
 
 @app.get("/health")
