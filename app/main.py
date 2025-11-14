611334349fe28c7d0978e18ed16577f67fb5a089
gaaljaylaani <GAALJAYLAANI123@GMAIL.COM>
2025-11-13 21:50:23 -0800

    add Tailwind CSS configuration and PostCSS setup for client app

--- app/main.py
+++ app/main.py
@@ -4,10 +4,26 @@ from .core.database import Base, engine
 import app.api.v1.auth as auth
 import app.api.v1.users as users
 import app.api.v1.notes as notes
+from fastapi.middleware.cors import CORSMiddleware
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="AI Note Taker")
+
+
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(notes.router)
 app.include_router(users.router)
